8f7243e28b1f641306a7ba0d1b9f0a8993f505a9
Dayoon Lee <danadayoonlee@gmail.com>
2020-02-11 18:09:48 -0500

    initial commit

---
+++ Question_Bank/src/questionBank/QuestionBankTest.java
@@ -0,0 +1,13 @@
+<<<<<<< HEAD:Question_Bank/src/questionBank/QuestionBankTest.java
+package questionBank;
+=======
+package question_bank;
+>>>>>>> 8e84186eb5df60fb6999c5b5714f3dcf7c1c18f4:question_bank/src/question_bank/QuestionBankTest.java
+
+public class QuestionBankTest {
+
+	public static void main(String[] args) {
+		QuestionBank questionBank = new QuestionBank();
+		questionBank.inputAnswer();
+	}
+}
