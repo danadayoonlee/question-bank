aae4557ab147957ef6ba7c82cba8412a37de4ad9
Dayoon Lee <danadayoonlee@gmail.com>
2020-02-11 18:11:38 -0500

    initial commit

---
+++ Question_Bank/src/question_bank/QuestionBankTest.java
@@ -0,0 +1,9 @@
+package question_bank;
+
+public class QuestionBankTest {
+
+	public static void main(String[] args) {
+		QuestionBank questionBank = new QuestionBank();
+		questionBank.inputAnswer();
+	}
+}
